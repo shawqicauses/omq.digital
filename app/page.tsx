19b1cc5eaefffbdaa2f904a83b8cfb7abd1a2b81
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-20 19:07:07 +0200

    Docs: Upload initial website files/folders

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {FormBasic} from "../examples"
+
+const Page = function Page() {
+  return (
+    <main>
+      <div className="mx-auto max-w-md py-20">
+        <FormBasic />
+      </div>
+    </main>
+  )
+}
+
+export default Page
