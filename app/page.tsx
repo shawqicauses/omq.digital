496ffbf4f58a6c00dedfd109ffeec25081c4e175
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-20 19:34:47 +0200

    Update: Enhance through deleting examples folder and updating `page.tsx` file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-// DONE REVIEWING: GITHUB COMMIT
-import {FormBasic} from "../examples"
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 
 const Page = function Page() {
   return (
     <main>
-      <div className="mx-auto max-w-md py-20">
-        <FormBasic />
-      </div>
+      OMQ Solutions Agency
     </main>
   )
 }
--- examples/index.ts
+++
@@ -1,3 +0,0 @@
-// DONE REVIEWING: GITHUB COMMIT
-export {default as FormBasic} from "./forms/basic"
-export default {}
