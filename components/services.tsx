dcc1f83a555956401dbca82f5866f881e6b5c610
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-21 20:15:33 +0200

    New: Enhance through adding `Services` UI data

--- components/services.tsx
+++ components/services.tsx
@@ -1,8 +1,8 @@
-// DONE REVIEWING: GITHUB COMMIT 2️⃣
+// DONE REVIEWING: GITHUB COMMIT 3️⃣
 
 import {
   ChartBarIcon,
-  CodeBracketIcon,
+  CodeBracketSquareIcon,
   DevicePhoneMobileIcon,
   SwatchIcon
 } from "@heroicons/react/24/solid"
@@ -13,7 +13,7 @@ const services = [
     title: "Web Development",
     description:
       "We create stunning, responsive websites that provide an intuitive user experience. Using the latest technologies, we ensure your site is fast, secure, and scalable.",
-    icon: CodeBracketIcon
+    icon: CodeBracketSquareIcon
   },
   {
     id: 2,
@@ -43,7 +43,7 @@ const Services = function Services() {
     <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-xl-7 px-6 lg:px-8">
         <div className="mx-auto max-w-xl-2 lg:text-center">
-          <h2 className="text-base font-semi-bold leading-7 text-violet-500">Services</h2>
+          <h2 className="text-base font-medium leading-7 text-primary">Services</h2>
           <p className="mt-2 text-xl-3 font-bold tracking-tight text-white sm:text-xl-4">
             Transforming Your Digital Presence
           </p>
@@ -51,6 +51,21 @@ const Services = function Services() {
             OMQ Solutions offers services to elevate your brand and drive your business forward.
             Discover how we can help you achieve your goals.
           </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-xl-2 sm:mt-20 lg:mt-24 lg:max-w-xl-4">
+          <dl className="grid max-w-xl grid-cols-2 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {services.map((service) => (
+              <div key={service.id} className="relative pl-16">
+                <dt className="text-base font-semi-bold leading-7 text-white">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary shadow-xl shadow-violet-500/20">
+                    <service.icon aria-hidden="true" className="h-6 w-6 text-white" />
+                  </div>
+                  {service.title}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-slate-400">{service.description}</dd>
+              </div>
+            ))}
+          </dl>
         </div>
       </div>
     </div>
