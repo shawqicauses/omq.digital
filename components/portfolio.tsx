273ad484dc8a33aa410071c6c5454b69d4297c3f
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-24 18:33:06 +0200

    New: Enhance through adding `Portfolio` data

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -1,4 +1,30 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+const portfolios = [
+  {
+    id: 0,
+    image: "/assets/portfolio-01.webp",
+    title: "E-Commerce Platform",
+    testimonial:
+      "Working with OMQ Solutions was a game-changer for our business. Their expertise and dedication helped us launch a successful e-commerce platform that exceeded our expectations.",
+    client: {
+      name: "John Doe",
+      title: "CEO, Shop Online"
+    }
+  },
+  {
+    id: 1,
+    image: "/assets/portfolio-02.webp",
+    title: "Corporate Website Re-Design",
+    testimonial:
+      "The team at OMQ Solutions transformed our out-dated website into a modern, user-friendly site. Their attention to detail and creative approach made all the difference.",
+    client: {
+      name: "Jane Smith",
+      title: "Marketing Director, Tech-Corp"
+    }
+  }
+]
+
 const Portfolio = function Portfolio() {
   return <div>Portfolio</div>
 }
