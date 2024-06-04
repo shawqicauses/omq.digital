63e40237a98b0bbcbe10bce8a64dbcb2f6e9f363
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-04 22:16:15 +0200

    New: Enhance through adding `MarketPlace` introduction

--- components/market-place.tsx
+++ components/market-place.tsx
@@ -1,6 +1,24 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 const MarketPlace = function MarketPlace() {
-  return <div>Market-Place</div>
+  return (
+    <div className="bg-slate-950 py-24">
+      <div className="mx-auto max-w-xl-7 sm:px-6 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-slate-900 px-6 py-20 sm:rounded-xl sm:px-10 sm:py-24 xl:px-24">
+          <div className="mx-auto grid max-w-xl-2 grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-y-0">
+            <div className="lg:row-start-2 lg:max-w-md">
+              <h2 className="text-xl-3 font-bold tracking-tight text-white sm:text-xl-4">
+                Power Your Business With Our Solutions.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-slate-300">
+                Discover the powerful features of our market-place system designed to grow small
+                businesses and provide everything for them.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default MarketPlace
