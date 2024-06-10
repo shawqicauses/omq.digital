658f738fa0a303355440c12a1f4a6978cb0b2d9c
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-10 04:24:06 +0200

    Update: Enhance through editing `MarketPlace` UI style

--- components/market-place.tsx
+++ components/market-place.tsx
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT 4️⃣
+// DONE REVIEWING: GITHUB COMMIT 5️⃣
 import {
   BuildingStorefrontIcon,
   CreditCardIcon,
@@ -27,8 +27,8 @@ const features = [
 const MarketPlace = function MarketPlace() {
   return (
     <div className="bg-slate-950 py-24">
-      <div className="mx-auto max-w-xl-7 sm:px-6 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-slate-900 px-6 py-20 sm:rounded-xl sm:px-10 sm:py-24 xl:px-24">
+      <div className="mx-auto max-w-xl-7 px-6 lg:px-8">
+        <div className="relative isolate overflow-hidden">
           <div className="mx-auto grid max-w-xl-2 grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-y-0">
             <div className="lg:row-start-2 lg:max-w-md">
               <h2 className="text-xl-3 font-bold tracking-tight text-white sm:text-xl-4">
@@ -44,7 +44,7 @@ const MarketPlace = function MarketPlace() {
               alt="Market-Place Screen Shot"
               width={2432}
               height={1442}
-              className="relative -z-20 min-w-full max-w-xl rounded-xl shadow-xl ring-1 ring-white/10 lg:row-span-4 lg:w-[64rem] lg:max-w-none"
+              className="relative min-w-full rounded-xl shadow-xl ring-1 ring-white/10 lg:row-span-4 lg:w-[64rem]"
             />
             <div className="max-w-xl lg:row-start-3 lg:mt-10 lg:max-w-md lg:border-t lg:border-white/10 lg:pt-10">
               <dl className="max-w-xl space-y-8 text-base leading-7 text-slate-300 lg:max-w-none">
