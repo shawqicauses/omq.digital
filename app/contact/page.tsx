a0e45d14c6adfdedfe96ceafc4a8a7f88ce28baf
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-12 16:51:55 +0200

    New: Enhance through adding contact blur effect

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,9 @@
-// DONE REVIEWING: GITHUB COMMIT 1️⃣
+// DONE REVIEWING: GITHUB COMMIT 2️⃣
 const Page = function Page() {
   const id = "contact-pattern"
+  const clipPath =
+    "polygon(74% 56%, 100% 38%, 97% 73%, 85% 100%, 80% 98%, 72% 67%, 60% 37%, 52% 32%, 47% 41%, 45% 65%, 27% 23%, 0% 35%, 17% 0%, 27% 23%, 76% 2%, 74% 56%)"
+
   return (
     <div className="relative isolate">
       <div className="mx-auto grid max-w-xl-7 grid-cols-1 lg:grid-cols-2">
@@ -22,10 +25,18 @@ const Page = function Page() {
                   </pattern>
                 </defs>
                 <svg x="100%" y={-1} className="overflow-visible fill-slate-800/20">
-                  <path strokeWidth={0} fill={`url(#${id})`} d="M-470.5 0h201v201h-201Z" />
-                  <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id})`} />
+                  <path strokeWidth={0} d="M-470.5 0h201v201h-201Z" />
                 </svg>
+                <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id})`} />
               </svg>
+              <div
+                aria-hidden="true"
+                className="absolute -left-56 top-[calc(100%-13rem)] transform-gpu blur-xl-3 lg:left-[max(-14rem,calc(100%-59rem))] lg:top-[calc(50%-7rem)]">
+                <div
+                  className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-br from-violet-400 to-blue-400 opacity-20"
+                  style={{clipPath}}
+                />
+              </div>
             </div>
           </div>
         </div>
