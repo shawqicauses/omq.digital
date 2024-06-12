2ef065a325b8b3b840484dab37b9bfdc31b43825
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-12 20:40:11 +0200

    New: Enhance through adding `contacts`

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,35 @@
-// DONE REVIEWING: GITHUB COMMIT 2️⃣
+// DONE REVIEWING: GITHUB COMMIT 3️⃣
+
+import {BuildingOffice2Icon, EnvelopeIcon, PhoneIcon} from "@heroicons/react/24/outline"
+import {Fragment} from "react"
+
+const address = (
+  <Fragment>
+    Our company is located <br /> in Bahrain
+  </Fragment>
+)
+
+const contacts = [
+  {
+    icon: BuildingOffice2Icon,
+    name: "Address",
+    title: address,
+    href: "https:// maps.google.com"
+  },
+  {
+    icon: PhoneIcon,
+    name: "Phone",
+    title: "+0 (123) 456-7890",
+    href: "tel:+1234567890"
+  },
+  {
+    icon: EnvelopeIcon,
+    name: "Email",
+    title: "contact@omq.com",
+    href: "mailto:contact@omq.com"
+  }
+]
+
 const Page = function Page() {
   const id = "contact-pattern"
   const clipPath =
@@ -38,6 +69,30 @@ const Page = function Page() {
                 />
               </div>
             </div>
+            <h2 className="text-xl-3 font-bold tracking-tight text-white">Get in touch</h2>
+            <p className="mt-6 text-lg leading-8 text-slate-400">
+              Do you have any questions or would like to work together? We would love to hear from
+              you and get to know you better.
+            </p>
+            <dl className="mt-10 space-y-4 text-base leading-7 text-slate-300">
+              {contacts.map((element) => (
+                <div key={element.name} className="flex gap-x-4">
+                  <dt className="flex-none">
+                    <span className="sr-only">{element.name}</span>
+                    <element.icon aria-hidden="true" className="h-7 w-6 text-slate-400" />
+                  </dt>
+                  <dd>
+                    <a
+                      href={element.href}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover:text-white">
+                      {element.title}
+                    </a>
+                  </dd>
+                </div>
+              ))}
+            </dl>
           </div>
         </div>
       </div>
