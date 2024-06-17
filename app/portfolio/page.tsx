9aeff44b641ef35ce301bfca37f9024ca3605608
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-18 00:37:13 +0200

    New: Enhance through adding portfolio page

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,17 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {Fragment} from "react"
+import {Container, SectionHeader} from "../../components/index"
+
+const PortfolioPage = function PortfolioPage() {
+  return (
+    <Fragment>
+      <SectionHeader
+        title="Portfolio"
+        description="Discover our latest projects and see how we can help you achieve your goals and drive your business forward with our expert solutions."
+      />
+      <Container>Portfolio</Container>
+    </Fragment>
+  )
+}
+
+export default PortfolioPage
