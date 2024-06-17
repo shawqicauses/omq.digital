3ee27030f2dd68c2c326f5e6cad5a6302862e466
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-17 21:22:18 +0200

    New: Enhance through adding `title` and `description` props to `<SectionHeader />`

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,14 @@
-// DONE REVIEWING: GITHUB COMMIT 1️⃣
+// DONE REVIEWING: GITHUB COMMIT 2️⃣
 import {Fragment} from "react"
 import {SectionHeader} from "../../components/index"
 
 const ServicesPage = function ServicesPage() {
   return (
     <Fragment>
-      <SectionHeader />
+      <SectionHeader
+        title="Services"
+        description="Discover how we can help you achieve your goals and drive your business forward with our expert services and solutions."
+      />
       <main>Services Page</main>
     </Fragment>
   )
