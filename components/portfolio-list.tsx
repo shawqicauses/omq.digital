8ec7307f4af84b832223b3c0483230016a948395
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-18 00:56:27 +0200

    New: Enhance through adding `PortfolioListProps`

--- components/portfolio-list.tsx
+++ components/portfolio-list.tsx
@@ -1,5 +1,19 @@
-// DONE REVIEWING: GITHUB COMMIT
-const PortfolioList = function PortfolioList() {
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+type PortfolioListProps = {
+  portfolios: {
+    id: string | number
+    image: string
+    title: string
+    testimonial: string
+    client: {
+      name: string
+      title: string
+    }
+  }[]
+}
+
+const PortfolioList = function PortfolioList({portfolios}: PortfolioListProps) {
   return <div>Portfolio List</div>
 }
 
