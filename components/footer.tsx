b56607ee47402111f73538461b9f2e0d6ae89fd7
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-17 18:01:43 +0200

    New: Enhance through adding `socials` to `Footer`

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,24 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+import {BsFacebook, BsInstagram, BsTwitter} from "react-icons/bs"
+
+const socials = [
+  {
+    name: "Facebook",
+    href: "https://facebook.com/omqsolutions",
+    icon: BsFacebook
+  },
+  {
+    name: "Instagram",
+    href: "https://instagram.com/omqsolutions",
+    icon: BsInstagram
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/omqsolutions",
+    icon: BsTwitter
+  }
+]
+
 const Footer = function Footer() {
   return <footer>Footer</footer>
 }
