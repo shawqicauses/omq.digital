e420aa28e89688993f2a3356cae444548d191c81
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-18 23:28:45 +0200

    Fix: Enhance through fixing colors in `Hero`

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT 3️⃣
+// DONE REVIEWING: GITHUB COMMIT 4️⃣
 
 import {ChevronRightIcon} from "@heroicons/react/24/outline"
 import Image from "next/image"
@@ -28,15 +28,15 @@ const Hero = function Hero() {
         aria-hidden="true"
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-xl-3 sm:-top-80">
         <div
-          className={`relative left-[calc(${classes.calc[1].base})] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-violet-400 to-blue-400 opacity-20 sm:left-[calc(${classes.calc[1].sm})] sm:w-[72.1875rem]`}
+          className={`relative left-[calc(${classes.calc[1].base})] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-primary to-secondary opacity-20 sm:left-[calc(${classes.calc[1].sm})] sm:w-[72.1875rem]`}
           style={{clipPath: classes.clipPath}}
         />
       </div>
       <div className="mx-auto max-w-xl-2 py-32 sm:py-48 lg:py-56">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div className="shc-flex-center shc-transition relative gap-2 rounded-full bg-slate-800 bg-opacity-30 px-3 py-1 text-sm leading-6 text-slate-400 ring-1 ring-slate-800 backdrop-blur-lg !transition-all hover:ring-white">
+          <div className="shc-flex-center shc-transition relative gap-2 rounded-full bg-border bg-opacity-30 px-3 py-1 text-sm leading-6 text-muted-foreground ring-1 ring-border backdrop-blur-lg !transition-all hover:ring-foreground">
             Announcing our last out-standing project.
-            <Link href="/portfolio" className="shc-flex-center font-medium text-white">
+            <Link href="/portfolio" className="shc-flex-center font-medium text-foreground">
               <span aria-hidden="true" className="absolute inset-0" />
               View more{" "}
               <ChevronRightIcon aria-hidden="true" className="h-4 w-4 stroke-2 text-current" />
@@ -44,14 +44,14 @@ const Hero = function Hero() {
           </div>
         </div>
         <div className="text-center">
-          <h1 className="text-xl-4 font-bold tracking-tight text-white sm:text-xl-6">
+          <h1 className="text-xl-4 font-bold tracking-tight text-foreground sm:text-xl-6">
             Transforming Digital{" "}
             <span className="underline decoration-primary decoration-dotted underline-offset-4">
               Dreams
             </span>{" "}
             into Reality
           </h1>
-          <p className="mt-6 text-lg leading-8 text-slate-300">
+          <p className="mt-6 text-lg leading-8 text-muted-foreground">
             OMQ Solutions is your trusted partner in innovative web development, seam-less digital
             marketing, and cutting-edge technology solutions.
           </p>
@@ -60,7 +60,7 @@ const Hero = function Hero() {
               <Link href="/contact">Get Started</Link>
             </Button>
             <Button variant="link" asChild>
-              <Link href="/about" className="text-white">
+              <Link href="/about" className="text-foreground">
                 About us
               </Link>
             </Button>
@@ -71,7 +71,7 @@ const Hero = function Hero() {
         aria-hidden="true"
         className={`absolute inset-x-0 top-[calc(${classes.calc[2].base})] -z-10 transform-gpu overflow-hidden blur-xl-3 sm:top-[calc(${classes.calc[2].sm})]`}>
         <div
-          className={`relative left-[calc(${classes.calc[3].base})] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-violet-400 to-blue-400 opacity-20 sm:left-[calc(${classes.calc[3].sm})] sm:w-[72.1875rem]`}
+          className={`relative left-[calc(${classes.calc[3].base})] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-primary to-secondary opacity-20 sm:left-[calc(${classes.calc[3].sm})] sm:w-[72.1875rem]`}
           style={{clipPath: classes.clipPath}}
         />
       </div>
