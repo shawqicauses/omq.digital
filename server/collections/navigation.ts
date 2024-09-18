56895a6d8bb82f3ae8dc15ebc19df8fa5967f961
Shawqi Hatem <shawqicauses@outlook.com>
2024-09-18 23:50:45 +0200

    New: Enhance through adding `navigationCollection`

---
+++ server/collections/navigation.ts
@@ -0,0 +1,29 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {CollectionConfig} from "payload/types"
+
+const navigationCollection: CollectionConfig = {
+  slug: "navigations",
+  labels: {
+    singular: {en: "Navigation", ar: "التنقل"},
+    plural: {en: "Navigations", ar: "التنقلات"}
+  },
+  admin: {group: {en: "Website Content", ar: "محتوى الموقع"}},
+  fields: [
+    {
+      label: {en: "Name", ar: "الاسم"},
+      type: "text",
+      name: "name",
+      required: true,
+      localized: true
+    },
+    {
+      label: {en: "URL", ar: "الرابط"},
+      type: "text",
+      name: "href",
+      required: true,
+      localized: true
+    }
+  ]
+}
+
+export default navigationCollection
