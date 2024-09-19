cfe4b02e28949429d4421331aabafa5eb7bfff91
Shawqi Hatem <shawqicauses@outlook.com>
2024-09-19 22:04:38 +0200

    Update: Enhance through adding `next-intl` to `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-// DONE REVIEWING: GITHUB COMMIT 2️⃣
+// DONE REVIEWING: GITHUB COMMIT 3️⃣
 /** @type {import("next").NextConfig} */
+const createNextIntlPlugin = require("next-intl/plugin")
+
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig = {images: {domains: ["tailwindui.com", "images.unsplash.com"]}}
 
-module.exports = nextConfig
+module.exports = withNextIntl(nextConfig)
