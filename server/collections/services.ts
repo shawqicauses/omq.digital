af680367e9d75d7f389adc1c0ed22215b3678dc7
Shawqi Hatem <shawqicauses@outlook.com>
2024-10-09 17:44:34 +0200

    New: Enhance through adding `upload` field to services collection

--- server/collections/services.ts
+++ server/collections/services.ts
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
 import {CollectionConfig} from "payload/types"
 
 const ServicesCollection: CollectionConfig = {
@@ -8,6 +8,19 @@ const ServicesCollection: CollectionConfig = {
     plural: {en: "Services", ar: "خدمات"}
   },
   admin: {group: {en: "Website Content", ar: "محتوى الموقع"}},
+  upload: {
+    staticURL: "/media",
+    staticDir: "media",
+    mimeTypes: ["image/png", "image/jpeg", "image/svg"],
+    imageSizes: [
+      {
+        name: "icon",
+        width: 24,
+        height: 24,
+        position: "center"
+      }
+    ]
+  },
   fields: [
     {
       label: {en: "Title", ar: "العنوان"},
