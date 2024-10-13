dfa74907370ab2fec615a6c554eb32831e4601f6
Shawqi Hatem <shawqicauses@outlook.com>
2024-10-13 02:10:35 +0200

    New: Enhance through adding `app/providers.tsx`

---
+++ app/providers.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT
+
+import trpc from "@/client"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import {httpBatchLink} from "@trpc/client"
+import {PropsWithChildren, useState} from "react"
+
+const createQueryClient = function createQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000
+      }
+    }
+  })
+}
+
+let browserQueryClient: QueryClient | undefined
+
+const getQueryClient = function getQueryClient() {
+  if (typeof window !== "undefined") return createQueryClient()
+  if (!browserQueryClient) browserQueryClient = createQueryClient()
+  return browserQueryClient
+}
+
+const Providers = function Providers({children}: PropsWithChildren) {
+  const queryClient = getQueryClient()
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
+          fetch(url, options) {
+            return fetch(url, {...options, credentials: "include"})
+          }
+        })
+      ]
+    })
+  )
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  )
+}
+
+export default Providers
--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,6 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
 import {AppRouter} from "@/server/api"
 import {createTRPCReact} from "@trpc/react-query"
 
-const trpc = createTRPCReact<AppRouter>()
+const trpc = createTRPCReact<AppRouter>({})
 export default trpc
