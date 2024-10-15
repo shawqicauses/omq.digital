f80357d09ef81abeb8f53e5181aa2c4c476d301f
Shawqi Hatem <shawqicauses@outlook.com>
2024-10-15 02:18:23 +0200

    New: Enhance through fetching/displaying services data in services page

--- app/[locale]/services/page.tsx
+++ app/[locale]/services/page.tsx
@@ -1,17 +1,35 @@
-// DONE REVIEWING: GITHUB COMMIT - 06
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT - 07
+import trpc from "@/client"
+import {Service} from "@/payload-types"
+import {Loader2Icon} from "lucide-react"
+import {useTranslations} from "next-intl"
 import {Fragment} from "react"
 import {Container, SectionHeader, ServicesList} from "../../../components/index"
-import {services} from "../../../components/services"
 
-const ServicesPage = function ServicesPage() {
+const ServicesPage = function ServicesPage({params: {locale}}: {params: {locale: string}}) {
+  const {data, isLoading} = trpc.getServices.useQuery({locale})
+
+  const services = data?.services.map((service) => ({
+    id: service.id,
+    title: service.title,
+    description: service.description,
+    icon: service.icon
+  }))
+
+  const t = useTranslations("home-page.services")
+
   return (
     <Fragment>
-      <SectionHeader
-        title="Services"
-        description="Discover how we can help you achieve your goals and drive your business forward with our expert services and solutions."
-      />
+      <SectionHeader title={t("title")} description={t("quote")} />
       <Container>
-        <ServicesList services={services} />
+        {(isLoading && (
+          <div className="mt-16 flex h-full min-h-screen items-center justify-center text-primary">
+            <Loader2Icon className="h-10 w-10 animate-spin stroke-current" />
+          </div>
+        )) ||
+          (services && <ServicesList locale={locale} services={services as Service[]} />)}
       </Container>
     </Fragment>
   )
