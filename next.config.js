de3e7ebfc964062f3333368246cc28d75804a49c
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-04 01:33:08 +0200

    New: Enhance through `images.remotePatterns` in `next.config.js`

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
-// DONE REVIEWING: GITHUB COMMIT 3️⃣
+// DONE REVIEWING: GITHUB COMMIT - 04
 /** @type {import("next").NextConfig} */
 const createNextIntlPlugin = require("next-intl/plugin")
 
 const withNextIntl = createNextIntlPlugin()
 
-const nextConfig = {images: {domains: ["tailwindui.com", "images.unsplash.com"]}}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**"
+      }
+    ]
+  }
+}
 
 module.exports = withNextIntl(nextConfig)
