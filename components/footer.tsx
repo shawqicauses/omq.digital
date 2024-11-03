d6a214f8828487e0b9e8e060901660f9b33c9ada
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-04 01:46:13 +0200

    Update: Enhance through updating footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,8 @@
-// DONE REVIEWING: GITHUB COMMIT - 03
+// DONE REVIEWING: GITHUB COMMIT - 04
+import {useTranslations} from "next-intl"
+import {Fragment} from "react"
 import {BsFacebook, BsInstagram, BsTwitter} from "react-icons/bs"
+import {Logo} from "./navigation"
 
 const socials = [
   {
@@ -20,29 +23,36 @@ const socials = [
 ]
 
 const Footer = function Footer() {
+  const t = useTranslations("home-page.footer")
   return (
-    <footer className="border-t border-t-muted bg-muted/25">
-      <div className="mx-auto max-w-xl-7 px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center gap-x-6 md:order-2">
-          {socials.map((social) => (
-            <a
-              key={social.name}
-              href={social.href}
-              target="_blank"
-              rel="noreferrer"
-              className="shc-transition text-muted-foreground hover:text-foreground">
-              <span className="sr-only">{social.name}</span>
-              <social.icon aria-hidden="true" className="h-5 w-5" />
-            </a>
-          ))}
-        </div>
-        <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-xs leading-5 text-muted-foreground">
-            &copy; 2024 OMQ Solutions, Inc. Rights Reserved.
-          </p>
-        </div>
+    <Fragment>
+      <div className="mx-auto mb-20 flex max-w-xl-2 flex-col items-center justify-center">
+        <Logo className="!h-40" />
+        <p className="text-center text-lg leading-8 text-muted-foreground">{t("paragraph")}</p>
       </div>
-    </footer>
+      <footer className="border-t border-t-muted bg-muted/25">
+        <div className="mx-auto max-w-xl-7 px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center gap-x-6 md:order-2">
+            {socials.map((social) => (
+              <a
+                key={social.name}
+                href={social.href}
+                target="_blank"
+                rel="noreferrer"
+                className="shc-transition text-muted-foreground hover:text-foreground">
+                <span className="sr-only">{social.name}</span>
+                <social.icon aria-hidden="true" className="h-5 w-5" />
+              </a>
+            ))}
+          </div>
+          <div className="mt-8 md:order-1 md:mt-0">
+            <p className="text-center text-xs leading-5 text-muted-foreground">
+              &copy; 2024 OMQ Solutions, Inc. Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </Fragment>
   )
 }
 
