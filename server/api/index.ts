1bc7e21d100e3bdedfee622450626e8f9f4280fe
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-05 22:24:17 +0200

    New: Enhance through adding about API

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,10 +1,27 @@
-// DONE REVIEWING: GITHUB COMMIT - 01
+// DONE REVIEWING: GITHUB COMMIT - 02
 import {TRPCError} from "@trpc/server"
 import {z} from "zod"
 import initPayload from "../payload"
 import {procedure, router} from "./trpc"
 
 export const appRouter = router({
+  getAbouts: procedure
+    .input(z.object({locale: z.string().min(1).max(64)}))
+    .query(async ({input: {locale}}) => {
+      const payload = await initPayload()
+
+      try {
+        const {docs: abouts} = await payload.find({
+          collection: "abouts",
+          sort: "createdAt",
+          locale
+        })
+
+        return {success: true, abouts}
+      } catch (error) {
+        throw new TRPCError({code: "INTERNAL_SERVER_ERROR"})
+      }
+    }),
   getServices: procedure
     .input(z.object({locale: z.string().min(1).max(64)}))
     .query(async ({input: {locale}}) => {
