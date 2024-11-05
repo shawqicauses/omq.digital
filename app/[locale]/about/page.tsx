a72eb2f98b502824622b74a618eab59ddce40ec5
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-05 15:26:34 +0200

    New: Enhance through adding background effect to about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,6 +1,21 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
 const Page = function Page() {
-  return <main>About</main>
+  const clipPath =
+    "polygon(73% 51%, 91% 11%, 100% 46%, 97% 82%, 92% 84%, 75% 64%, 75% 64%, 55% 47%, 46% 49%, 45% 62%, 50% 87%, 21% 64%, 0% 100%, 5% 51%, 21% 63%, 58% 0%, 73% 51%)"
+
+  return (
+    <main className="relative isolate">
+      {/* Background */}
+      <div
+        aria-hidden="true"
+        className="fixed inset-x-0 top-4 -z-10 flex transform-gpu justify-center overflow-hidden blur-xl-3">
+        <div
+          className="aspect-[1108/632] w-[69.25rem] flex-none transform-gpu bg-gradient-to-r from-primary to-secondary opacity-25"
+          style={{clipPath}}
+        />
+      </div>
+    </main>
+  )
 }
 
 export default Page
