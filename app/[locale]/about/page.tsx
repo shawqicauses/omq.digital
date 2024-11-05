1d16df3feca7136ae9b598b13117360aac62c0b2
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-05 16:50:47 +0200

    New: Enhance through adding title and description to about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,5 +1,10 @@
-// DONE REVIEWING: GITHUB COMMIT - 01
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT - 02
+import {useTranslations} from "next-intl"
+
 const Page = function Page() {
+  const t = useTranslations()
   const clipPath =
     "polygon(73% 51%, 91% 11%, 100% 46%, 97% 82%, 92% 84%, 75% 64%, 75% 64%, 55% 47%, 46% 49%, 45% 62%, 50% 87%, 21% 64%, 0% 100%, 5% 51%, 21% 63%, 58% 0%, 73% 51%)"
 
@@ -13,6 +18,18 @@ const Page = function Page() {
           className="aspect-[1108/632] w-[69.25rem] flex-none transform-gpu bg-gradient-to-r from-primary to-secondary opacity-25"
           style={{clipPath}}
         />
+      </div>
+
+      {/* Header */}
+      <div className="px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-xl-2 pt-24 text-center sm:pt-40">
+          <h2 className="text-xl-4 font-bold tracking-tight text-foreground sm:text-xl-6">
+            {t("about.title")}.
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-muted-foreground">
+            {t("home-page.header.description")}
+          </p>
+        </div>
       </div>
     </main>
   )
