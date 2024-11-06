fed6eb008d26e703605af6175e9b3bea3e38efbb
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-06 21:23:01 +0200

    New: Enhance through adding loading state to service page

--- app/[locale]/services/[id]/page.tsx
+++ app/[locale]/services/[id]/page.tsx
@@ -1,12 +1,20 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT - 01
+// DONE REVIEWING: GITHUB COMMIT - 02
 
 import trpc from "@/client"
 import {Service} from "@/payload-types"
+import {Loader2Icon} from "lucide-react"
 
 const Page = function Page({params: {id, locale}}: {params: {id: string; locale: string}}) {
   const {data, isLoading} = trpc.getService.useQuery({id, locale})
+
+  if (isLoading || !data.service)
+    return (
+      <div className="my-16 flex h-full min-h-screen items-center justify-center text-primary">
+        <Loader2Icon className="h-10 w-10 animate-spin stroke-current" />
+      </div>
+    )
 
   const service = (
     data.service.map((element) => ({
@@ -19,7 +27,7 @@ const Page = function Page({params: {id, locale}}: {params: {id: string; locale:
 
   return (
     <div className="flex h-screen items-center justify-center text-foreground">
-      Service {id} in {locale}
+      Service {service.title} in {locale}
     </div>
   )
 }
