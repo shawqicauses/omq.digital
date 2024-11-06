8f26906325f900428b8a008fe1a3004c4e8cc1f8
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-06 21:18:44 +0200

    New: Enhance through fetching service data

--- app/[locale]/services/[id]/page.tsx
+++ app/[locale]/services/[id]/page.tsx
@@ -1,6 +1,22 @@
-// DONE REVIEWING: GITHUB COMMIT
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT - 01
+
+import trpc from "@/client"
+import {Service} from "@/payload-types"
 
 const Page = function Page({params: {id, locale}}: {params: {id: string; locale: string}}) {
+  const {data, isLoading} = trpc.getService.useQuery({id, locale})
+
+  const service = (
+    data.service.map((element) => ({
+      id: element.id,
+      title: element.title,
+      description: element.description,
+      icon: element.icon
+    })) as Service[]
+  )[0]
+
   return (
     <div className="flex h-screen items-center justify-center text-foreground">
       Service {id} in {locale}
