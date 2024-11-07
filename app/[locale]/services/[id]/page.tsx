2e7f23f2ba54f072f6ec68bdb9802d72c2e965f1
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-07 02:35:30 +0200

    New: Enhance through adding packages to service page

--- app/[locale]/services/[id]/page.tsx
+++ app/[locale]/services/[id]/page.tsx
@@ -1,13 +1,18 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT - 03
+// DONE REVIEWING: GITHUB COMMIT - 04
 
 import trpc from "@/client"
+import {Button} from "@/components/ui"
 import {Media, Service} from "@/payload-types"
-import {Loader2Icon} from "lucide-react"
+import {CheckIcon, Loader2Icon} from "lucide-react"
+import {useTranslations} from "next-intl"
 import Image from "next/image"
+import Link from "next/link"
+import {Fragment} from "react"
 
 const Page = function Page({params: {id, locale}}: {params: {id: string; locale: string}}) {
+  const t = useTranslations("navigation")
   const clipPath =
     "polygon(73% 51%, 91% 11%, 100% 46%, 97% 82%, 92% 84%, 75% 64%, 75% 64%, 55% 47%, 46% 49%, 45% 62%, 50% 87%, 21% 64%, 0% 100%, 5% 51%, 21% 63%, 58% 0%, 73% 51%)"
 
@@ -25,12 +30,13 @@ const Page = function Page({params: {id, locale}}: {params: {id: string; locale:
       id: element.id,
       title: element.title,
       description: element.description,
-      icon: element.icon
+      icon: element.icon,
+      packages: element.packages
     })) as Service[]
   )[0]
 
   return (
-    <main className="relative isolate mx-auto mb-24 mt-48 w-full max-w-xl-7 px-6 lg:px-8">
+    <main className="max-w-xl-8 relative isolate mx-auto mb-24 mt-48 w-full px-6 lg:px-8">
       {/* Background */}
       <div
         aria-hidden="true"
@@ -42,11 +48,11 @@ const Page = function Page({params: {id, locale}}: {params: {id: string; locale:
       </div>
 
       {/* Header */}
-      <header>
-        <div className="mx-auto mb-10 flex h-28 w-28 items-center justify-center rounded-xl border border-primary bg-muted/25 shadow-xl shadow-primary/20">
+      <header className="mb-24">
+        <div className="mx-auto mb-10 flex h-28 w-28 items-center justify-center rounded-lg border border-primary bg-background shadow-xl shadow-primary/20">
           <Image
             src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/${(service.icon as Media).sizes.thumbnail_tablet.filename}`}
-            alt={(service.icon as Media).sizes.thumbnail.filename}
+            alt={service.title}
             fill
             sizes="1.5rem"
             className="!static !h-auto !w-16"
@@ -59,6 +65,65 @@ const Page = function Page({params: {id, locale}}: {params: {id: string; locale:
           {service.description}
         </p>
       </header>
+
+      <p className="mb-24 text-center text-lg font-medium italic text-foreground">
+        {locale === "ar" ? (
+          <Fragment>اختر باقة {service.title} التي تناسب ميزانيتك.</Fragment>
+        ) : locale === "en" ? (
+          <Fragment>Choose the {service.title} package that fits your budget.</Fragment>
+        ) : null}
+      </p>
+
+      {/* Packages */}
+      <div>
+        {[...service.packages, ...service.packages, ...service.packages].map((element, index) => (
+          <div key={element.id + "-" + index} className="mb-24 flex flex-col items-center">
+            <h2 className="mb-3 text-center text-base font-medium leading-7 text-primary">
+              {element.title}
+            </h2>
+            <p className="mb-6 flex items-baseline gap-x-2">
+              <span className="text-xl-5 font-bold tracking-tight text-foreground">
+                {element.pricing.pricing_from} - {element.pricing.pricing_to}
+              </span>
+              <span className="text-base uppercase tracking-widest text-muted-foreground">
+                / {{en: "BHD", ar: "د.ب"}[locale]}
+              </span>
+            </p>
+            <Button size="lg" asChild className="mb-12">
+              <Link href="http://wa.me/97339999728" target="_blank">
+                {t("request-package")}
+              </Link>
+            </Button>
+            <div className="grid w-full grid-cols-1 items-start gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {element.offerings.map((offering) => (
+                <div
+                  key={offering.id}
+                  className="relative rounded-lg bg-background p-3 shadow-xl-2 ring-1 ring-border sm:p-6">
+                  <h3 className="mb-4 text-xl font-medium !leading-relaxed text-foreground">
+                    {offering.title}
+                  </h3>
+                  <p className="mb-4 flex items-baseline gap-x-2 leading-none">
+                    <span className="text-xl font-medium tracking-tight text-foreground">
+                      {offering.pricing.pricing_from} - {offering.pricing.pricing_to}
+                    </span>
+                    <span className="text-base uppercase tracking-widest text-muted-foreground">
+                      / {{en: "BHD", ar: "د.ب"}[locale]}
+                    </span>
+                  </p>
+                  <ul className="mt-5 space-y-3 text-sm leading-6 text-muted-foreground">
+                    {offering.features.map((feature) => (
+                      <li key={feature.id} className="flex gap-x-3">
+                        <CheckIcon aria-hidden="true" className="h-6 w-5 flex-none text-primary" />
+                        <span>{feature.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
