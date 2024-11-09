2b06d8cb7fc8692a00283e165f6467acdf62c933
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-09 18:43:33 +0200

    Update: Enhance through updating services component

--- components/services.tsx
+++ components/services.tsx
@@ -1,43 +1,30 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT - 11
+// DONE REVIEWING: GITHUB COMMIT - 12
 
-import trpc from "@/client"
-import {Service} from "@/payload-types"
-import {Loader2Icon} from "lucide-react"
 import {useTranslations} from "next-intl"
 import Container from "./container"
 import ServicesList from "./services-list"
 
-const Services = function Services({locale}: {locale: string}) {
-  const {data, isLoading} = trpc.getServices.useQuery({locale})
-
-  const services = data
-    ? (data.services.map((doc) => ({
-        id: doc.id,
-        title: doc.title,
-        description: doc.description,
-        icon: doc.icon
-      })) as Service[])
-    : []
-
+const Services = function Services({
+  locale,
+  asSection = false
+}: {
+  locale: string
+  asSection?: boolean
+}) {
   const t = useTranslations("home-page.services")
-
   return (
-    <div className="pt-24 sm:pt-32">
+    <div className="my-16 md:my-32">
       <Container>
-        <div className="mx-auto max-w-xl-2 lg:text-center">
-          <h2 className="text-base font-medium leading-7 text-primary">{t("headline")}</h2>
-          <p className="mt-2 text-xl-3 font-bold tracking-tight text-foreground sm:text-xl-4">
+        <div className="mx-0 max-w-none lg:mx-auto lg:max-w-xl-2 lg:text-center">
+          <h2 className="mb-2 text-base font-medium leading-7 text-primary">{t("headline")}</h2>
+          <p className="mb-4 text-xl-3 font-bold tracking-tight text-foreground sm:text-xl-4">
             {t("title")}
           </p>
-          <p className="mt-6 text-lg italic leading-8 text-muted-foreground">{t("quote")}</p>
+          <p className="text-lg italic leading-8 text-muted-foreground">{t("quote")}</p>
         </div>
-        {((isLoading || !services.length) && (
-          <div className="mt-16 flex items-center justify-center text-primary">
-            <Loader2Icon className="h-10 w-10 animate-spin stroke-current" />
-          </div>
-        )) || <ServicesList services={services} />}
+        <ServicesList locale={locale} asSection={asSection} />
       </Container>
     </div>
   )
