8a70a2cfb29f63a3149f0dd6a4d421ee557e11c1
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-09 20:27:37 +0200

    Fix: Enhance through fixing `tailwind-merge`

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
+import {twMerge} from "@/tailwind.config"
 import {clsx, type ClassValue} from "clsx"
-import {twMerge} from "tailwind-merge"
 
 export const cn = function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
