50b63d7471d9cdb74847f4f4c29ecb7a9f2a8279
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-09 20:19:39 +0200

    Update: Enhance through updating services page

--- app/[locale]/services/page.tsx
+++ app/[locale]/services/page.tsx
@@ -1,35 +1,18 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT - 10
-import trpc from "@/client"
-import {Service} from "@/payload-types"
-import {Loader2Icon} from "lucide-react"
+// DONE REVIEWING: GITHUB COMMIT - 11
 import {useTranslations} from "next-intl"
 import {Fragment} from "react"
 import {Container, SectionHeader, ServicesList} from "../../../components/index"
 
 const ServicesPage = function ServicesPage({params: {locale}}: {params: {locale: string}}) {
-  const {data, isLoading} = trpc.getServices.useQuery({locale})
-
-  const services = data?.services.map((service) => ({
-    id: service.id,
-    title: service.title,
-    description: service.description,
-    icon: service.icon
-  })) as Service[]
-
   const t = useTranslations("home-page.services")
 
   return (
     <Fragment>
       <SectionHeader title={t("title")} description={t("description")} />
       <Container>
-        {(isLoading && (
-          <div className="mt-16 flex h-full min-h-screen items-center justify-center text-primary">
-            <Loader2Icon className="h-10 w-10 animate-spin stroke-current" />
-          </div>
-        )) ||
-          (services && <ServicesList services={services} />)}
+        <ServicesList locale={locale} asSection={false} />
       </Container>
     </Fragment>
   )
