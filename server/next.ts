3ff498a51d1daf67186d740ec1e4745e64135212
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-10 21:43:36 +0200

    Update: Enhance through updating `next.ts` file

--- server/next.ts
+++ server/next.ts
@@ -1,5 +1,9 @@
-// DONE REVIEWING: GITHUB COMMIT 1️⃣
+// DONE REVIEWING: GITHUB COMMIT - 02
+import dotenv from "dotenv"
 import next from "next"
+import path from "path"
+
+dotenv.config({path: path.resolve(__dirname, "../.env")})
 
 export const dev = process.env.NODE_ENV !== "production"
 export const port = Number(process.env.PORT) || 3000
